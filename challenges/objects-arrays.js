8ab9a5496d4dddc5023cb3702e6f94c25094d14a
Psage23 <pamelasage3@gmail.com>
2019-10-18 14:55:06 -0700

    completed objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -96,7 +96,8 @@ graduates.forEach(graduates => {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+
+const unisWithUni = universities.filter(name => name.includes('Uni'));
 
 console.log(unisWithUni);
 
